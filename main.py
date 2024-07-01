0cceae239f81b4f763ef0a18f6cafc714b6d2b35
manorajesh <mano@raje.sh>
2024-07-01 15:38:44 -0700

    Refactor protoype into a class for clarity

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 from selenium import webdriver
+from urllib.parse import quote_plus
 
-url = 'https://www.indeed.com/jobs?q=&l=El+Segundo%2C+CA&radius=10&advn=6787486936730117&sort=date&vjk=0b710fbedcd006ce'
-browser = webdriver.Firefox()
-browser.get(url)
+class IndeedScraper:
+    def __init__(self, radius=10, location='El Segundo, CA'):
+        self.url = f'https://www.indeed.com/jobs?q=&l={quote_plus(location)}&radius={radius}&sort=date'
+        print(self.url)
+        self.radius = radius
+        self.location = location
+        self.browser = webdriver.Firefox()
+        self.browser.get(self.url)
 
-assert 'El Segundo, CA' in browser.title
+    def get_jobs(self):
+        div = self.browser.find_element('id', 'mosaic-provider-jobcards')
+        lis = div.find_elements('tag name', 'li')
 
-div = browser.find_element('id', 'mosaic-provider-jobcards')
-ul = div.find_element('tag name', 'ul')
-lis = ul.find_elements('tag name', 'li')
+        # Get the title of each job in list
+        for li in lis:
+            try:
+                title = li.find_element('tag name', 'span').text
+                if title != '':
+                    print(title)
+            except Exception as e:
+                title = 'N/A'
 
-for li in lis:
-    try:
-        title = li.find_element('tag name', 'span').text
-        if title != '':
-            print(title)
-    except Exception as e:
-        title = 'N/A'
+    def close_browser(self):
+        self.browser.quit()
+    
+
+if __name__ == '__main__':
+    scraper = IndeedScraper()
+    scraper.get_jobs()
+    scraper.close_browser()
