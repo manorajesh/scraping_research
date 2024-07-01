e091ad075705077925fa7d9b7ee8a5d3d7e98ed3
manorajesh <mano@raje.sh>
2024-07-01 16:14:13 -0700

    Added measures to prevent bot detection and more robust logging

--- main.py
+++ main.py
@@ -1,50 +1,79 @@
+import time
+import random
+import logging
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from urllib.parse import quote_plus
+from fake_useragent import UserAgent
 
 class IndeedScraper:
-    def __init__(self, radius=10, location='El Segundo, CA', start=0):        
+    def __init__(self, radius=10, location='El Segundo, CA', start=0):
         self.radius = radius
         self.location = location
         self.start = start
-
+        self.user_agent = UserAgent()
         self.assemble_url()
-        self.browser = webdriver.Firefox()
+        self.browser = self.init_browser()
         self.browser.get(self.url)
+
+    def init_browser(self):
+        options = Options()
+        options.set_preference("general.useragent.override", self.get_random_user_agent())
+        options.headless = True
+        return webdriver.Firefox(options=options)
+
+    def get_random_user_agent(self):
+        user_agent = self.user_agent.random
+        while 'bot' in user_agent or 'spider' in user_agent:
+            user_agent = self.user_agent.random
+
+        logger.info(f"Using user agent: {user_agent}")
+        return user_agent
+
+    def random_delay(self, min_seconds=1, max_seconds=5):
+        logger.info(f"Sleeping for {min_seconds} to {max_seconds} seconds")
+        time.sleep(random.uniform(min_seconds, max_seconds))
 
     def assemble_url(self):
         self.url = f'https://www.indeed.com/jobs?q=&l={quote_plus(self.location)}&radius={self.radius}&sort=date&start={self.start}'
-        print(f'Using url: {self.url}')
+        logger.info(f"Assembled URL: {self.url}")
 
-    def get_jobs(self):
+    def get_jobs(self) -> None:
+        self.random_delay()
         try:
             div = self.browser.find_element('id', 'mosaic-provider-jobcards')
             lis = div.find_elements('tag name', 'li')
         except Exception as e:
-            print('No more jobs found: ', e)
+            logger.error(f"Error: {e}")
             return
 
         # Get the title of each job in list
         for li in lis:
+            self.random_delay()
             try:
                 title = li.find_element('tag name', 'span').text
                 if title != '':
                     print(title)
             except Exception as e:
+                logger.error(f"Error: {e}")
                 title = 'N/A'
 
         self.next_page()
 
     def next_page(self):
+        logger.info(f"Scraped {self.start} jobs. Next page.")
         self.start += 10
         self.assemble_url()
         self.browser.get(self.url)
         self.get_jobs()
 
     def close_browser(self):
+        logger.info("Closing browser")
         self.browser.quit()
-    
 
+logger = logging.getLogger(__name__)
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     scraper = IndeedScraper()
     scraper.get_jobs()
     scraper.close_browser()
