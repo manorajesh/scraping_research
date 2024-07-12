8a1cfd5f1c1c7b9b4e70254c506db3b7a665e591
manorajesh <mano@raje.sh>
2024-07-11 23:03:18 -0700

    Added prototype code for Milvus vector db I/O

--- job-scraper/src/main/java/com/projectleo/scraper/database/Database.java
+++ job-scraper/src/main/java/com/projectleo/scraper/database/Database.java
@@ -2,82 +2,173 @@ package com.projectleo.scraper.database;
 
 import com.projectleo.scraper.scrapers.JobResult;
 import com.projectleo.scraper.util.PropertiesUtil;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import io.milvus.v2.client.ConnectConfig;
+import io.milvus.v2.client.MilvusClientV2;
+import io.milvus.v2.common.DataType;
+import io.milvus.v2.common.IndexParam;
+import io.milvus.v2.service.collection.request.AddFieldReq;
+import io.milvus.v2.service.collection.request.CreateCollectionReq;
+import io.milvus.v2.service.collection.request.GetLoadStateReq;
+import io.milvus.v2.service.collection.request.HasCollectionReq;
+import io.milvus.v2.service.collection.request.ReleaseCollectionReq;
+import io.milvus.v2.service.vector.request.SearchReq;
+import io.milvus.v2.service.vector.response.SearchResp;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Database implements AutoCloseable {
   private static final Logger logger = LogManager.getLogger(Database.class);
-  private Connection connection;
+  private static final String COLLECTION_NAME = "job_results";
+  private static final String CLUSTER_ENDPOINT = PropertiesUtil.getProperty("milvus.endpoint");
+  private static final int HASH_LENGTH = 256; // 256 bits for SHA-256 hash
+  private static final int VECTOR_DIMENSION = 3072; // for OpenAI's text-embedding-3-large
+  private final MilvusClientV2 client;
 
   public Database() {
-    try {
-      String jdbcUrl = PropertiesUtil.getProperty("jdbc.url");
-      String jdbcUser = PropertiesUtil.getProperty("jdbc.user");
-      String jdbcPassword = PropertiesUtil.getProperty("jdbc.password");
+    ConnectConfig connectConfig = ConnectConfig.builder().uri(CLUSTER_ENDPOINT).build();
 
-      this.connection = DriverManager.getConnection(jdbcUrl, jdbcUser, jdbcPassword);
-    } catch (SQLException e) {
-      logger.error("Error initializing database connection: {}", e.getMessage());
-      throw new RuntimeException("Error initializing database connection", e);
+    client = new MilvusClientV2(connectConfig);
+
+    createCollectionIfNotExists();
+  }
+
+  private void createCollectionIfNotExists() {
+    if (!client.hasCollection(HasCollectionReq.builder().collectionName(COLLECTION_NAME).build())) {
+      createCollection();
     }
   }
 
-  public void writeToDatabase(List<JobResult> jobResults) {
-    String insertSql =
-        "INSERT INTO job_results (company, job_title, industry, responsibilities, qualifications,"
-            + " skills, location, job_link_hash) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-    try (PreparedStatement insertStmt = connection.prepareStatement(insertSql)) {
-      logger.info("Writing {} job results to database", jobResults.size());
-      for (JobResult jobResult : jobResults) {
-        // No need to check if job link exists in database since it is already checked in the
-        // scraper
-        insertStmt.setString(1, jobResult.getCompany());
-        insertStmt.setString(2, jobResult.getJobTitle());
-        insertStmt.setString(3, jobResult.getIndustry());
-        insertStmt.setString(4, String.join("|", jobResult.getResponsibilities()));
-        insertStmt.setString(5, String.join("|", jobResult.getQualifications()));
-        insertStmt.setString(6, String.join("|", jobResult.getSkills()));
-        insertStmt.setString(7, jobResult.getLocation());
-        insertStmt.setBytes(8, jobResult.getJobLinkHash());
-        // TODO: Not setting timestamp in code. Field has CURRENT_TIMESTAMP as default value
-        insertStmt.addBatch();
-      }
-      insertStmt.executeBatch();
-      logger.info("Successfully wrote job results to database");
-    } catch (Exception e) {
-      logger.error("Error writing to database: {}", e.getMessage());
+  private void createCollection() {
+    // 1. Create collection schema
+    CreateCollectionReq.CollectionSchema schema = client.createSchema();
+
+    // id
+    schema.addField(
+        AddFieldReq.builder()
+            .fieldName("id")
+            .dataType(DataType.Int64)
+            .isPrimaryKey(true)
+            .autoID(true)
+            .build());
+
+    // vector
+    schema.addField(
+        AddFieldReq.builder()
+            .fieldName("vector")
+            .dataType(DataType.FloatVector)
+            .dimension(VECTOR_DIMENSION) // 512 dimensions for 512-bit vectors
+            .build());
+
+    // jobLinkHash
+    schema.addField(
+        AddFieldReq.builder()
+            .fieldName("job_link_hash")
+            .dataType(DataType.BinaryVector)
+            .dimension(HASH_LENGTH) // 256 bits for SHA-256 hash
+            .build());
+
+    // timestamp
+    schema.addField(AddFieldReq.builder().fieldName("timestamp").dataType(DataType.Int64).build());
+
+    // company
+    schema.addField(AddFieldReq.builder().fieldName("company").dataType(DataType.String).build());
+
+    // jobTitle
+    schema.addField(AddFieldReq.builder().fieldName("job_title").dataType(DataType.String).build());
+
+    // industry
+    schema.addField(AddFieldReq.builder().fieldName("industry").dataType(DataType.String).build());
+
+    // responsibilities
+    schema.addField(
+        AddFieldReq.builder().fieldName("responsibilities").dataType(DataType.Array).build());
+
+    // qualifications
+    schema.addField(
+        AddFieldReq.builder().fieldName("qualifications").dataType(DataType.Array).build());
+
+    // skills
+    schema.addField(AddFieldReq.builder().fieldName("skills").dataType(DataType.Array).build());
+
+    // location
+    schema.addField(AddFieldReq.builder().fieldName("location").dataType(DataType.String).build());
+
+    // 2. Prepare index parameters
+    IndexParam indexParamForIdField =
+        IndexParam.builder().fieldName("id").indexType(IndexParam.IndexType.AUTOINDEX).build();
+
+    IndexParam indexParamForVectorField =
+        IndexParam.builder()
+            .fieldName("vector")
+            .indexType(IndexParam.IndexType.IVF_FLAT)
+            .metricType(IndexParam.MetricType.COSINE)
+            .extraParams(Map.of("nlist", 1024))
+            .build();
+
+    List<IndexParam> indexParams = new ArrayList<>();
+    indexParams.add(indexParamForIdField);
+    indexParams.add(indexParamForVectorField);
+
+    // 3 Create a collection with schema and index parameters
+    CreateCollectionReq customizedSetupReq1 =
+        CreateCollectionReq.builder()
+            .collectionName(COLLECTION_NAME)
+            .collectionSchema(schema)
+            .indexParams(indexParams)
+            .build();
+
+    client.createCollection(customizedSetupReq1);
+
+    // Check if the collection is created
+    boolean res =
+        client.getLoadState(GetLoadStateReq.builder().collectionName(COLLECTION_NAME).build());
+
+    if (res) {
+      logger.info("Collection created successfully");
+    } else {
+      logger.error("Collection failed to be created");
+      throw new RuntimeException("Collection failed to be created");
     }
   }
+
+  public void writeToDatabase(List<JobResult> jobResults) {}
 
   public boolean jobLinkExists(byte[] jobLinkHash) {
-    String checkSql = "SELECT COUNT(*) FROM job_results WHERE job_link_hash = ?";
-    try (PreparedStatement checkStmt = connection.prepareStatement(checkSql)) {
-      checkStmt.setBytes(1, jobLinkHash);
-      try (ResultSet resultSet = checkStmt.executeQuery()) {
-        if (resultSet.next()) {
-          return resultSet.getInt(1) > 0;
-        }
-      }
-    } catch (SQLException e) {
-      logger.error("Error checking job link hash in database: {}", e.getMessage());
-    }
-    return false;
+    List<Object> vectorList = new ArrayList<>();
+    vectorList.add(jobLinkHash);
+
+    SearchReq searchReq =
+        SearchReq.builder()
+            .collectionName(COLLECTION_NAME)
+            .annsField("job_link_hash")
+            .topK(1)
+            .data(Collections.singletonList(vectorList))
+            .build();
+
+    SearchResp searchResp = client.search(searchReq);
+
+    return !searchResp.getSearchResults().isEmpty();
   }
 
   @Override
   public void close() {
-    try {
-      if (connection != null && !connection.isClosed()) {
-        connection.close();
-      }
-    } catch (SQLException e) {
-      logger.error("Error closing database connection: {}", e.getMessage());
+    ReleaseCollectionReq releaseCollectionReq =
+        ReleaseCollectionReq.builder().collectionName(COLLECTION_NAME).build();
+
+    client.releaseCollection(releaseCollectionReq);
+
+    boolean res =
+        client.getLoadState(GetLoadStateReq.builder().collectionName(COLLECTION_NAME).build());
+
+    if (!res) {
+      logger.info("Collection released successfully");
+    } else {
+      logger.error("Collection failed to be released");
+      throw new RuntimeException("Collection failed to be released");
     }
   }
 }
